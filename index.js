d486cd7ba5c28bbd1c8493bc2b24d096cf7862e5
tripathysagar <tripathysagar08@gmail.com>
2023-12-04 12:26:23 +0530

    after adding healthcheck

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
+app.use(cors())
+
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/health', (req, res) => {
+    res.send('I am ok!!')
 })
 
 app.listen(port, () => {
